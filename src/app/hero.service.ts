80f39b5e41872161122092d1beaf76ae1bd88971
MrVVoodu <jota.sanches@sapo.pt>
2022-06-28 18:08:48 +0100

    First branch commit

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,29 +1,33 @@
 import { Injectable } from '@angular/core';
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { Hero, IAPI } from './hero';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 import { MessageService } from './message.service';
 
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpContext, HttpHeaders, HttpResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  private heroesUrl = 'api/heroex';  // URL to web api
-  
+  private heroesUrl = 'http://localhost:8080/heroes';  // URL to web api
+  private baseURL = 'https://api.github.com/users/MrVVoodu/repos'; // TESTE: APAGAR
+
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
-  
+
   /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroesUrl)
+    // return this.http.get<Hero[]>(this.baseURL, this.httpOptions)
+    return this.http.get<IAPI>(this.heroesUrl, this.httpOptions)
       .pipe(
-        tap(_ => this.log('fetched heroes')),
-        catchError(this.handleError<Hero[]>('getHeroes', []))
+        map( (resp: IAPI) => {
+          this.log('fetched heroes');
+          return resp._embedded.heroList
+        }),
+        catchError(this.handleError<any>('getHeroes DEBUG', []))
       );
   }
 
@@ -34,19 +38,19 @@ export class HeroService {
  * @param operation - name of the operation that failed
  * @param result - optional value to return as the observable result
  */
-private handleError<T>(operation = 'operation', result?: T) {
-  return (error: any): Observable<T> => {
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
 
-    // TODO: send the error to remote logging infrastructure
-    console.error(error); // log to console instead
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
 
-    // TODO: better job of transforming error for user consumption
-    this.log(`${operation} failed: ${error.message}`);
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
 
-    // Let the app keep running by returning an empty result.
-    return of(result as T);
-  };
-}
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 
   /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
@@ -64,7 +68,9 @@ private handleError<T>(operation = 'operation', result?: T) {
 
   /** PUT: update the hero on the server */
   updateHero(hero: Hero): Observable<any> {
-    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+    const url = `${this.heroesUrl}/${hero.id}`;
+
+    return this.http.put(url, hero, this.httpOptions).pipe(
       tap(_ => this.log(`updated hero id=${hero.id}`)),
       catchError(this.handleError<any>('updateHero'))
     );
@@ -72,7 +78,9 @@ private handleError<T>(operation = 'operation', result?: T) {
 
   /** POST: add a new hero to the server */
   addHero(hero: Hero): Observable<Hero> {
-    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+    const url = `${this.heroesUrl}/${hero.id}`;
+
+    return this.http.post<Hero>(url, hero, this.httpOptions).pipe(
       tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
       catchError(this.handleError<Hero>('addHero'))
     );
--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,4 +1,13 @@
 export interface Hero {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
+}
+
+
+export interface IAPI {
+  _embedded: {
+    heroList: Hero[];
+    _links: any;
   }
+  _links: any;
+}
